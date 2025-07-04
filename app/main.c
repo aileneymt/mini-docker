cf38ae45b6b656d2f0a393969682fd91a90c5014
Ailene Tan <aileneymt@gmail.com>
2025-07-04 10:58:13 -0400

    Working on stage 2

--- app/main.c
+++ app/main.c
@@ -4,7 +4,14 @@
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 int main(int argc, char *argv[]) {
-	setbuf(stdout, NULL);
+	int fd[2];
+  if (pipe(fd) == -1) {
+    // pipe failed
+    exit(1);
+  }
+  
+
+  setbuf(stdout, NULL);
  	setbuf(stderr, NULL);
 	char *command = argv[3];
 	int child_pid = fork();
@@ -13,10 +20,16 @@ int main(int argc, char *argv[]) {
 	    return 1;
 	}
 	if (child_pid == 0) {
-		   // Replace current program with calling program
-	    execv(command, &argv[3]);
+		// Replace current program with calling program
+	  close(fd[0]); // close read end
+    dup2(fd[1], STDOUT_FILENO); // make stdout point to the write end of the pipe
+    dup2(fd[1], STDERR_FILENO);
+    execv(command, &argv[3]);
+    
 	} else {
 		   // We're in parent
+       close(fd[1]); // close write end
+       dup2(fd[0], STDIN_FILENO); // make STDIN point to the same thing as the pipes read end 
 		   wait(NULL);
 		   printf("Child terminated");
 	}
