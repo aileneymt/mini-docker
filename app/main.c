ff097d38d85ebaf44fa20257b078e097eb7bae31
Ailene Tan <aileneymt@gmail.com>
2025-07-04 16:53:33 -0400

    Temporarily committing overcomplicated solution, need to simplify

--- app/main.c
+++ app/main.c
@@ -2,7 +2,76 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
+
+char * find_binary(const char *binary) {
+  char * path_env = getenv("PATH"); // returns colon separated string like /user/local/bin:/usr/bin:/bin
+  if (!path_env) return NULL;
+  
+  char * path_copy = strdup(path_env); // get a copy of the path
+  char *dir = strtok(path_copy, ":");
+
+  while (dir != NULL) { // iterate through each directory token
+    int path_size = strlen(dir) + strlen(binary) + 2;
+    char * full_path = malloc(path_size * sizeof(char));
+
+    snprintf(full_path, path_size, "%s/%s", dir, binary); // assemble the potential path to binary
+    
+    if (access(full_path, X_OK) == 0) { // file exists and is executable
+      free(path_copy);
+      return full_path;
+    }
+    free(full_path);
+    dir = strtok(NULL, ":");
+  }
+  free(path_copy);
+  return NULL;
+
+}
+
+
+int copy_file(const char *src_path, const char *dest_path)
+{
+  FILE * src = fopen(src_path, "rb");
+  FILE * dest = fopen(dest_path, "wb");
+  if (!src) {
+    printf("Source file: %s\n", src_path);
+    perror("Failed to open source file");
+    return 1;
+  }
+  if (!dest) {
+    perror("Failed to open destination file.");
+    return 1;
+  }
+  
+  char buffer[4096];
+  size_t bytesRead;
+  while ((bytesRead = fread(buffer, 1, sizeof(buffer), src)) > 0) {
+    if (fwrite(buffer, sizeof(char), bytesRead, dest) != bytesRead) {
+      perror("Error while writing to destination file.");
+      fclose(src);
+      fclose(dest);
+      unlink(dest_path); // get rid of the incomplete file
+      return 1;
+    }
+  }
+
+  if (chmod(dest_path, 0755) != 0) {
+    perror("Failed to set file permissions");
+    return 1;
+  } // make sure the copied file is executable
+
+  fclose(src);
+  fclose(dest);
+  return 0;
+}
+
+
+
 int main(int argc, char *argv[])
 {
 
@@ -21,12 +90,11 @@ int main(int argc, char *argv[])
   shell_args[3] = NULL;
 
   // assemble the full command
-  char full_cmd[1024] = {0};
-  for (int i = 3; i < argc; i++) {
+  char full_cmd[1024] = "/";
+  strcat(full_cmd, argv[3]);
+  for (int i = 4; i < argc; i++) {
+    strcat(full_cmd, " ");
     strcat(full_cmd, argv[i]);
-    if (i != argc - 1) {
-      strcat(full_cmd, " ");
-    }
   }
   shell_args[2] = full_cmd;
 
@@ -42,7 +110,40 @@ int main(int argc, char *argv[])
     close(fd[0]);               // close read end
     dup2(fd[1], STDOUT_FILENO); // make stdout point to the write end of the pipe
     dup2(fd[1], STDERR_FILENO);
-    // printf("FROM CHILD");
+
+    // create temporary directory
+    char dir_path[128] = "/tmp/container-XXXXXX";
+    if (mkdtemp(dir_path) == NULL) { // failed to create new temporary directory
+      return 1;
+    }
+    char temp_binary_path[128] = {0}; // path of the binary inside the temporary directory (dir_path + binary)
+    strcpy(temp_binary_path, dir_path);
+    temp_binary_path[strlen(temp_binary_path)] = '/';
+    strncat(temp_binary_path, argv[3], sizeof(temp_binary_path) - 1); // concatenate the binary to the end
+
+    // find and copy the binaries to temporary directory
+    char * bin_path = find_binary(argv[3]);
+    
+    if (copy_file(bin_path, temp_binary_path) != 0) {
+      printf("Failed to copy file.");
+      return 1;
+    }
+    free(bin_path);
+    char *direct_args[] = {temp_binary_path, "TEST", NULL};
+    execv(temp_binary_path, direct_args);
+
+    // call chroot to change root to temporary directory
+    if (chroot(dir_path) != 0) {
+      printf("chroot failed.\n");
+      return 1;
+    }
+    
+    // call chdir to set working directory inside chroot
+    if (chdir("/") != 0) {
+      printf("chdir failed.\n");
+      return 1;
+    }
+
     execvp("sh", shell_args);
 
   }
@@ -64,3 +165,4 @@ int main(int argc, char *argv[])
   }
   return 0;
 }
+
