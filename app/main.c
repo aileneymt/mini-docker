5593e2812940742ec4447a0341bee78b680d89ec
Ailene Tan <aileneymt@gmail.com>
2025-07-04 14:33:59 -0400

    Working on making the exit status thing also work

--- app/main.c
+++ app/main.c
@@ -3,44 +3,52 @@
 #include <unistd.h>
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
-int main(int argc, char *argv[]) {
-  
+int main(int argc, char *argv[])
+{
+
   setbuf(stdout, NULL);
- 	setbuf(stderr, NULL);
-	
+  setbuf(stderr, NULL);
+
   char *command = argv[3];
-	int fd[2];
-  if (pipe(fd) == -1) {
+  int fd[2];
+  if (pipe(fd) == -1)
+  {
     // pipe failed
     return 1;
   }
-  
-	int child_pid = fork();
-	if (child_pid == -1) {
-	    printf("Error forking!");
-	    return 1;
-	}
-	if (child_pid == 0) {
-		// Replace current program with calling program
-	  close(fd[0]); // close read end
+
+  int child_pid = fork();
+  if (child_pid == -1)
+  {
+    printf("Error forking!");
+    return 1;
+  }
+  if (child_pid == 0)
+  { // CHILD ------------------------------------
+
+    close(fd[0]);               // close read end
     dup2(fd[1], STDOUT_FILENO); // make stdout point to the write end of the pipe
     dup2(fd[1], STDERR_FILENO);
-    //printf("FROM CHILD");
+    // printf("FROM CHILD");
     execvp(command, &argv[3]);
-    
-	} else {
-		   // We're in parent
-       close(fd[1]); // close write end
-       dup2(fd[0], STDIN_FILENO); // make STDIN point to the same thing as the pipes read end 
-       char buffer[128];
-       int bytesRead = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
-       buffer[bytesRead] = 0;
-       printf("%s", buffer);
 
-       int status;
-		   waitpid(child_pid, &status, 0);
-       return WEXITSTATUS(status);
-		   //printf("Child terminated");
-	}
-	return 0;
+  }
+  else
+  { // PARENT -------------------------------------
+    close(fd[1]);              // close write end
+    dup2(fd[0], STDIN_FILENO); // make stdin point to the same thing as the pipes read end
+    char buffer[128] = {};
+    int bytesRead = 0;
+    while ((bytesRead = read(STDIN_FILENO, buffer, sizeof(buffer) - 1)) > 0) {
+      buffer[bytesRead] = 0;
+      printf("%s", buffer);
+    }
+    close(fd[0]);
+    
+    int status;
+    waitpid(child_pid, &status, 0);
+    printf("Child exited with status: %d\n", WEXITSTATUS(status));
+    return WEXITSTATUS(status);
+  }
+  return 0;
 }
