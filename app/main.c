e0ff3c0bd2604a1834c9f824ab94d843077a721f
Ailene Tan <aileneymt@gmail.com>
2025-07-04 12:04:33 -0400

    Actually reading in the parent, trying to figure out why my original approach wasn't working

--- app/main.c
+++ app/main.c
@@ -24,14 +24,19 @@ int main(int argc, char *argv[]) {
 	if (child_pid == 0) {
 		// Replace current program with calling program
 	  close(fd[0]); // close read end
-    dup2(STDOUT_FILENO, fd[1]); // make stdout point to the write end of the pipe
-    dup2(STDERR_FILENO, fd[1]);
+    dup2(fd[1], STDOUT_FILENO); // make stdout point to the write end of the pipe
+    dup2(fd[1], STDERR_FILENO);
+    //printf("FROM CHILD");
     execv(command, &argv[3]);
     
 	} else {
 		   // We're in parent
        close(fd[1]); // close write end
        dup2(fd[0], STDIN_FILENO); // make STDIN point to the same thing as the pipes read end 
+       char buffer[128];
+       int bytesRead = read(STDIN_FILENO, buffer, 10);
+       buffer[bytesRead] = 0;
+       printf("%s", buffer);
 		   wait(NULL);
 		   //printf("Child terminated");
 	}
